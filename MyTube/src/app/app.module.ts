b746f46e95ab243c2c13db6003524239ba8735b0
tyronmaster <degonski.art@gmail.com>
2023-10-23 14:22:54 +0200

    feat: add header section components

--- MyTube/src/app/app.module.ts
+++ MyTube/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchItemComponent } from './search/search-item/search-item.component';
 import { SearchResultListComponent } from './search/search-result-list/search-result-list.component';
+import { HeaderComponent } from './header/header/header.component';
+import { SearchBarComponent } from './header/search-bar/search-bar.component';
+import { AuthComponent } from './header/auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchItemComponent,
-    SearchResultListComponent
+    SearchResultListComponent,
+    HeaderComponent,
+    SearchBarComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
