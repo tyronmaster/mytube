e8e464d866a98b6ecf8fa8fcdd306f06a7e6c3f4
tyronmaster <degonski.art@gmail.com>
2023-11-02 20:24:57 +0100

    feat: add colored border logic

--- MyTube/src/app/search/search-item/search-item.component.ts
+++ MyTube/src/app/search/search-item/search-item.component.ts
@@ -11,4 +11,25 @@ import { SearchItem } from '../models/search-item.model';
 })
 export class SearchItemComponent {
   @Input() item!: SearchItem;
+
+  coloredAge: string = '';
+
+  ngOnInit() {
+    let nowDate = Date.now();
+    let postDate = Date.parse(this.item.snippet.publishedAt);
+    const postAge = nowDate - postDate;
+
+    this.coloredAge = this.ageParser(postAge);
+  }
+
+  ageParser(age: number): string {
+    if (age > 15778800000) {
+      return 'red';
+    } else if (age > 2629800000) {
+      return 'yellow';
+    } else if (age > 604800000) {
+      return 'green';
+    }
+    return 'blue';
+  }
 }
